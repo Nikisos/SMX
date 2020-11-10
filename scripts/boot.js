68e1e79e4a1fd9723c3da9cf4e77afb7d3e4b70c
Pietro303HD <47507131+Pietro303HD@users.noreply.github.com>
2020-11-10 14:53:01 -0300

    Create boot.js

---
+++ scripts/boot.js
@@ -0,0 +1,13 @@
+//obligatory commentttt
+Events.on(ClientLoadEvent, b => {
+    Vars.ui.showCustomConfirm(
+        "ShadowMod","shadowmod is the best"
+        "No",
+        "Yes",
+        ()=>{
+            print("[accent]Cursed mode not enabled[]"
+        )},
+        ()=>{
+            
+        });
+});
